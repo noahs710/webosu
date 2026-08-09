5e7eb6d32b2e06eebb2cd0841a75884569135b3d
noahs710 <noahserapilio@proton.me>
2026-08-09 04:30:57 -0400

    Fix case-sensitive imports for Linux deploy (router pages lowercase)

--- src/vue/router.js
+++ src/vue/router.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/", name: "home", component: () => import("./pages/Home.js") },
-  { path: "/browse", name: "browse", component: () => import("./pages/Browse.js") },
-  { path: "/hot", name: "hot", component: () => import("./pages/Hot.js") },
-  { path: "/new", name: "new", component: () => import("./pages/New.js") },
-  { path: "/search", name: "search", component: () => import("./pages/Search.js") },
-  { path: "/leaderboard", name: "leaderboard", component: () => import("./pages/Leaderboard.js") },
-  { path: "/profile", name: "profile", component: () => import("./pages/Profile.js") },
-  { path: "/settings", name: "settings", component: () => import("./pages/Settings.js") },
-  { path: "/skins", name: "skins", component: () => import("./pages/Skins.js") },
-  { path: "/liked", name: "liked", component: () => import("./pages/Liked.js") },
-  { path: "/history", name: "history", component: () => import("./pages/History.js") },
+  { path: "/", name: "home", component: () => import("./pages/home.js") },
+  { path: "/browse", name: "browse", component: () => import("./pages/browse.js") },
+  { path: "/hot", name: "hot", component: () => import("./pages/hot.js") },
+  { path: "/new", name: "new", component: () => import("./pages/new.js") },
+  { path: "/search", name: "search", component: () => import("./pages/search.js") },
+  { path: "/leaderboard", name: "leaderboard", component: () => import("./pages/leaderboard.js") },
+  { path: "/profile", name: "profile", component: () => import("./pages/profile.js") },
+  { path: "/settings", name: "settings", component: () => import("./pages/settings.js") },
+  { path: "/skins", name: "skins", component: () => import("./pages/skins.js") },
+  { path: "/liked", name: "liked", component: () => import("./pages/liked.js") },
+  { path: "/history", name: "history", component: () => import("./pages/history.js") },
 ];
 
 export const router = createRouter({
